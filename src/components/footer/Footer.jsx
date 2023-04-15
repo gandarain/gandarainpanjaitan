5c13ac0aaaeebd49f5423b39203a0babfae5575f
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2023-04-16 03:48:20 +0700

    Enhance Footer Link

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import constants from "@/constants";
 import styles from "./footer.module.css"
@@ -14,38 +14,59 @@ const {
   }
 } = constants;
 
-const renderListButton = () => (
+const activeLinkStyle = (activeIndex, index) => (
+  activeIndex === index ? `${styles.footer__link} ${styles.active__footer__link}` : styles.footer__link
+)
+
+const renderListButton = ({ activeIndex, setActiveIndex }) => (
   <ul className={styles.footer__list}>
-    {button.map(item => (
-      <li key={item.id}>
-        <a href={item.id} className={styles.footer__link}>{item.title}</a>
+    {button.map((item, index) => (
+      <li key={item.id} onClick={() => setActiveIndex(index)}>
+        <a
+          href={item.id}
+          className={activeLinkStyle(activeIndex, index)}>
+            {item.title}
+          </a>
       </li>
     ))}
   </ul>
 )
 
-const Footer = () => (
-  <footer className={styles.footer}>
-    <div className={`${styles.footer__container} container`}>
-      <h1 className={styles.footer__title}>{title}</h1>
-      {renderListButton()}
-      <div className={styles.footer__social}>
-        {social_media.map((item, index) => (
-          <a
-            key={index}
-            href={item.link}
-            className={styles.footer__social__link}
-            target="_blank"
-          >
-            <i className={`uil uil-${item.icon}`} />
-          </a>
-      ))}
+const useFooterHooks = () => {
+  const [activeIndex, setActiveIndex] = useState(0)
+
+  return {
+    activeIndex,
+    setActiveIndex
+  }
+}
+
+const Footer = () => {
+  const state = useFooterHooks()
+
+  return (
+    <footer className={styles.footer}>
+      <div className={`${styles.footer__container} container`}>
+        <h1 className={styles.footer__title}>{title}</h1>
+        {renderListButton(state)}
+        <div className={styles.footer__social}>
+          {social_media.map((item, index) => (
+            <a
+              key={index}
+              href={item.link}
+              className={styles.footer__social__link}
+              target="_blank"
+            >
+              <i className={`uil uil-${item.icon}`} />
+            </a>
+        ))}
+        </div>
+        <span className={styles.footer__copy}>
+          &#169;{copyright}
+        </span>
       </div>
-      <span className={styles.footer__copy}>
-        &#169;{copyright}
-      </span>
-    </div>
-  </footer>
-)
+    </footer>
+  )
+}
 
 export default Footer
